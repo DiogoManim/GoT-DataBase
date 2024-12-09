014fc84430ed7574e1bcebef7b99ab0b04bfed77
Sophia Cheto <sophiacheto@gmail.com>
2024-12-09 11:07:04 +0000

    updates

--- app.py
+++ app.py
@@ -1,21 +1,217 @@
-from flask import Flask, render_template
-import sqlite3
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+from flask import Flask, render_template, abort
+import logging
+import db
 
-conn = sqlite3.connect('game_of_thrones.db')
-app = Flask(__name__)
+APP = Flask(__name__)
 
-cur = conn.cursor()
-cur.row_factory = sqlite3.Row
+# Start Page
+@APP.route('/')
+def index():
+    stats={}
+    stats = db.execute('''
+    SELECT COUNT(*) AS Houses FROM Houses;
+    ''').fetchone()
 
-@app.route('/')
-def relations():
-    return render_template('index.html')
+    logging.info(stats)
 
-@app.route('/wars')
-def wars():
-    return render_template('wars.html')
+    return render_template('index.html',stats=stats)
 
-@app.route('/<name>')
+############ REGIÕES
+@APP.route('/regions')
+def regions():
+    regions = db.execute(
+        '''
+        SELECT Region, Region_ID
+        FROM Regions
+        ORDER BY Region
+        ''').fetchall()
+    return render_template('regions.html', regions=regions)
+
+@APP.route('/regions/<int:id>/')
+def get_region(id):
+    region = db.execute(
+        '''
+        SELECT Region_ID, Region, Description
+        FROM Regions
+        Where Region_ID = ?
+        ''', [id]).fetchone()
+    
+    if region is None:
+        abort(404, 'Region ID {} does not exist.'.format(id))
+
+    ruller = db.execute(
+        '''
+        SELECT House_ID, House
+        FROM Houses JOIN Regions 
+        ON House_ID = Rulled_by_House_ID
+        WHERE Region_ID = ?
+        ''', [id]).fetchone()
+    
+
+    return render_template('regionsid.html', region=region, ruller=ruller)
+
+############ CASAS
+@APP.route('/houses')
+def houses():
+    houses = db.execute(
+        '''
+        SELECT House, House_ID, Words
+        FROM Houses
+        ORDER BY House
+        ''').fetchall()
+    
+    qtd = db.execute(
+        '''
+        SELECT count(house_id) AS qtd
+        FROM houses;
+        '''
+        ).fetchone()
+    return render_template('houses.html', houses=houses, qtd=qtd)
+
+############ BATALHAS
+@APP.route('/battles')
+def battles():
+    battles = db.execute(
+        '''
+        SELECT battle_name, battle_ID, battle_type, year
+        FROM battles
+        ORDER BY year, battle_name
+        ''').fetchall()
+    
+    qtd = db.execute(
+        '''
+        SELECT count(battle_id) AS qtd
+        FROM battles;
+        '''
+        ).fetchone()
+    
+    return render_template('battles.html', battles=battles, qtd=qtd)
+
+@APP.route('/battles/<int:id>/')
+def get_battle(id):
+    battle = db.execute(
+        '''
+        SELECT battle_id as id, battle_name as name, year, is_attacker_winner, battle_type as type, major_death, major_capture, attacker_size, defender_size 
+        FROM battles
+        Where battle_ID = ?
+        ''', [id]).fetchone()
+    
+    if battle is None:
+        abort(404, 'battle ID {} does not exist.'.format(id))
+
+    region = db.execute(
+        '''
+        SELECT battle_name, region, region_id, battle_id
+        FROM battles NATURAL JOIN regions
+        WHERE battle_id = ?
+        ''', [id]).fetchone()
+    
+    attack_commanders = db.execute(
+        '''
+        select character, character_id
+        from characters join attack_commanders
+        on character_id=attackcommander_id
+        where battle_id=?
+        ''', [id]).fetchall()
+    
+    defense_commanders = db.execute(
+        '''
+        select character, character_id
+        from characters join defense_commanders
+        on character_id=defense_commander_id
+        where battle_id=?
+        ''', [id]).fetchall()
+    
+    attacker_houses = db.execute(
+        '''
+        select house, house_id
+        from attacks join houses
+        on house_id=attackerhouse_id
+        where battle_id=?
+        ''', [id]).fetchall()
+    
+    defender_houses = db.execute(
+        '''
+        select house, house_id
+        from defenses join houses
+        on house_id=defenderhouse_id
+        where battle_id=?
+        ''', [id]).fetchall()
+    
+    attacker_king = db.execute(
+        '''
+        select character, character_id, imageUrl
+        from battles join characters
+        on character_id = attackerking_id
+        where battle_id=?
+        ''', [id]).fetchone()
+    
+    defender_king = db.execute(
+        '''
+        select character, character_id, imageUrl
+        from battles join characters
+        on character_id = defenderking_id
+        where battle_id=?
+        ''', [id]).fetchone()
+    
+
+    return render_template('battlesid.html', battle=battle, defender_king=defender_king, attacker_king=attacker_king, region=region, attack_commanders=attack_commanders, defense_commanders=defense_commanders, attacker_houses=attacker_houses, defender_houses=defender_houses)
+
+
+
+
+############ CIDADES
+@APP.route('/cities')
+def cities():
+    cities = db.execute(
+        '''
+        SELECT city, region, city_id
+        FROM cities NATURAL JOIN regions
+        ORDER BY city
+        ''').fetchall()
+    
+    qtd = db.execute(
+        '''
+        SELECT count(city_id) AS qtd
+        FROM cities;
+        '''
+        ).fetchone()
+    
+    return render_template('cities.html', cities=cities, qtd=qtd)
+
+@APP.route('/cities/<int:id>/')
+def get_city(id):
+    city = db.execute(
+        '''
+        SELECT city_ID, city, region_id
+        FROM cities
+        Where city_ID = ?
+        ''', [id]).fetchone()
+    
+    if city is None:
+        abort(404, 'City ID {} does not exist.'.format(id))
+
+    region = db.execute(
+        '''
+        SELECT city, region, region_id, city_id
+        FROM cities NATURAL JOIN regions
+        WHERE city_id = ?
+        ''', [id]).fetchone()
+    
+    houses = db.execute(
+        '''
+        SELECT house_id, house
+        FROM houses
+        WHERE city_id = ?
+        ''', [id]).fetchall()
+
+    return render_template('citiesid.html', city=city, region=region, houses=houses)
+
+
+
+@APP.route('/<name>')
 def panel(name):
     try:
         return render_template(f'components/{name}/{name}.html')
@@ -25,4 +221,5 @@ def panel(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    db.connect()
+    APP.run(debug=True)
